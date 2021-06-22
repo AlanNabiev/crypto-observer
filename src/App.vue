317acf4e17ae90d742527bfe4d573a3679ce4f58
AlanNabiev <alannabiev2001@gmail.com>
2021-06-22 19:27:35 +0300

    Add watch and computed update

--- src/App.vue
+++ src/App.vue
@@ -142,30 +142,30 @@
       <template v-if="tickers.length">
         <div>
           <p class="text-sm font-medium text-gray-700">
-             Фильтр:
-          <input
-          v-model="filter"
-          class="
-                  block
-                  pr-10
-                  border-gray-300
-                  text-gray-900
-                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
-                  sm:text-sm
-                  rounded-md
-                " 
-          placeholder="Например B..."
-          />
+            Фильтр:
+            <input
+              v-model="filter"
+              class="
+                block
+                pr-10
+                border-gray-300
+                text-gray-900
+                focus:outline-none focus:ring-gray-500 focus:border-gray-500
+                sm:text-sm
+                rounded-md
+              "
+              placeholder="Например B..."
+            />
           </p>
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in filteredTickers()"
+            v-for="t in paginatedTickers"
             :key="t.name"
             @click="select(t)"
             :class="{
-              'border-4': sel === t,
+              'border-4': selectedTicker === t,
             }"
             class="
               bg-white
@@ -218,78 +218,86 @@
               >Удалить
             </button>
           </div>
-          
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
-        <div>
+        <div class="inline-flex">
           <button
-          @click="page = page - 1"
-          v-if="page>1"
-          type="button"
-          class="
-            my-4
-            inline-flex
-            items-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            leading-4
-            font-medium
-            rounded-full
-            text-white
-            bg-gray-600
-            hover:bg-gray-700
-            transition-colors
-            duration-300
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-gray-500
-          ">Назад</button>
+            @click="page = page - 1"
+            v-if="page > 1"
+            type="button"
+            class="
+              my-4
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
+          >
+            Назад
+          </button>
+          <dd class="mt-1 text-3xl font-semibold text-gray-900">
+            {{ page }}
+          </dd>
           <button
-          v-if="hasNextPage"
-          @click="page = page + 1"
-          type="button"
-          class="
-            my-4
-            inline-flex
-            items-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            leading-4
-            font-medium
-            rounded-full
-            text-white
-            bg-gray-600
-            hover:bg-gray-700
-            transition-colors
-            duration-300
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-gray-500
-          ">Вперед</button>
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            type="button"
+            class="
+              my-4
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
+          >
+            Вперед
+          </button>
         </div>
       </template>
-      <section v-if="sel" class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - USD
+          {{ selectedTicker.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-            v-for="(bar, idx) in normalizeGraph()"
+            v-for="(bar, idx) in normalizedGraph"
             :key="idx"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
           ></div>
         </div>
         <button
-          @click="sel = null"
+          @click="selectedTicker = null"
           type="button"
           class="absolute top-0 right-0"
         >
@@ -327,26 +335,27 @@ export default {
   data() {
     return {
       ticker: "",
+      filter: "",
       tickers: [],
-      sel: null,
       graph: [],
-      validateSel: Boolean,
+      selectedTicker: null,
+      validateSel: true,
       foundTokensRender: ["BTC", "ETH", "DOGE", "BCH"],
       pageLoading: "loading",
       page: 1,
-      filter: "",
-      hasNextPage: true,
     };
   },
 
   created() {
-    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
 
     if (windowData.filter) {
       this.filter = windowData.filter;
     }
 
-    if(windowData.page) {
+    if (windowData.page) {
       this.page = windowData.page;
     }
 
@@ -362,17 +371,50 @@ export default {
     }
   },
 
-  methods: {
-    filteredTickers() {
-      const start = ( this.page - 1 ) *6;
-      const end = this.page * 6;
-      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter.toUpperCase()));
-
-      this.hasNextPage = filteredTickers.length > end;
-
-      return filteredTickers.slice(start,end)
-    
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
     },
+
+    endIndex() {
+      return this.page * 6;
+    },
+
+    filteredTickers() {
+      return this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter.toUpperCase())
+      );
+    },
+
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+
+      if (maxValue === minValue) {
+        return this.graph.map(() => 50);
+      }
+
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+    pageStateOptions() {
+      return {
+        filter: this.filter,
+        page: this.page,
+      };
+    },
+  },
+
+  methods: {
     subscrideToUpdate(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -383,7 +425,7 @@ export default {
 
         this.tickers.find((t) => t.name === tickerName).price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === tickerName) {
+        if (this.selectedTicker?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 5000);
@@ -436,11 +478,11 @@ export default {
         name: this.ticker,
         price: "-",
       };
+
       this.tickerValidation();
-      localStorage.setItem("cryptocastle-list", JSON.stringify(this.tickers));
 
       if (this.ticker !== "" && this.validateSel !== false) {
-        this.tickers.push(currentTicker);
+        this.tickers = [...this.tickers, currentTicker];
         this.filter = "";
         this.subscrideToUpdate(currentTicker.name);
         this.ticker = "";
@@ -452,42 +494,44 @@ export default {
     },
 
     select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
+      this.selectedTicker = ticker;
     },
 
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null;
+      }
     },
-
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    }
   },
 
   watch: {
+    tickers() {
+      localStorage.setItem("cryptocastle-list", JSON.stringify(this.tickers));
+    },
+
+    selectedTicker() {
+      this.graph = [];
+    },
+
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },
+
     filter() {
       this.page = 1;
-      window.history.pushState(
-        null,
-        document.title, 
-        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
-        );
     },
-    page() {
+    pageStateOptions(value) {
       window.history.pushState(
         null,
-        document.title, 
-        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
-        );
-    }
+        document.title,
+        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
+      );
+    },
   },
 };
-
 </script>
 
 <style src="./assets/tailwind.css"></style>
