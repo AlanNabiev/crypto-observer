0a6c0ec51cc3770f9d3a632868c1c7854221cfe0
AlanNabiev <alannabiev2001@gmail.com>
2021-08-13 23:31:54 +0300

    feat: add quick searchig tokens list

--- src/components/common/AddToken.vue
+++ src/components/common/AddToken.vue
@@ -1,14 +1,53 @@
 <template>
-  <BaseInput
-    class="mt-10"
-    @keydown="add"
-    v-model="token"
-    label="Add token"
-    placeholder="Enter name or symbol..."
-  />
-  <BaseButton class="flex items-center my-10" @click="add">
-    <BaseIcon class="mr-2" name="plus-circle" />Add Token
-  </BaseButton>
+  <section class="px-44">
+    <BaseInput
+      class="mt-10"
+      @keydown.enter="add"
+      v-model="token"
+      label="Add token"
+      placeholder="Enter name or symbol..."
+    />
+    <BaseWarning
+      class="mr-96 mt-2"
+      v-if="!filteredTokens.length"
+      name="alert-triangle"
+      position="horizontal"
+      >Invalid token</BaseWarning
+    >
+    <ul class="mt-2 grid grid-flow-col grid-cols-4 grid-rows-1 gap-x-4">
+      <li
+        v-for="(tokenItem, idx) in filteredTokens"
+        :key="idx"
+        @click="addFromTokensList(tokenItem)"
+        class="
+          p-2
+          flex
+          justify-arouund
+          items-center
+          font-bold
+          text-white
+          shadow-xl
+          rounded-xl
+          hover:shadow-none
+          bg-purple-500
+          transition
+          delay-150
+          duration-300
+          ease-in-out
+          hover:bg-purple-800 hover:text-white
+          cursor-pointer
+        "
+      >
+        <img class="w-8 h-8 mr-2" :src="tokenItem.image" alt="" />
+        <span class="font-bold text-right">{{
+          tokenItem.symbol.toUpperCase()
+        }}</span>
+      </li>
+    </ul>
+    <BaseButton class="flex items-center m-10" @click="add">
+      <BaseIcon class="mr-2" name="plus-circle" />Add Token
+    </BaseButton>
+  </section>
 </template>
 
 <script lang="ts">
@@ -23,9 +62,17 @@ export default defineComponent({
       return store.getters["tokens/tokens"];
     }).value;
 
-    console.log(tokens.find((t) => t.Symbol === "ETH"));
-
     const token = ref("");
+
+    const filteredTokens = computed(() => {
+      return tokens
+        .filter(
+          (el) =>
+            el.name.toLowerCase().includes(token.value.toLowerCase()) ||
+            el.symbol.toLowerCase().includes(token.value.toLowerCase())
+        )
+        .slice(0, 4);
+    });
 
     function add(): void {
       if (token.value === "") {
@@ -35,10 +82,17 @@ export default defineComponent({
       token.value = "";
     }
 
+    function addFromTokensList(tokenItem) {
+      token.value = tokenItem.symbol.toUpperCase();
+      add();
+    }
+
     return {
       token,
       tokens,
-      add
+      add,
+      addFromTokensList,
+      filteredTokens
     };
   }
 });
