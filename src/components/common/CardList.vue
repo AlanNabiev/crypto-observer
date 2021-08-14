be646171fd6b6446ffa06e2916c846bc09d76831
AlanNabiev <alannabiev2001@gmail.com>
2021-08-14 18:26:33 +0300

    feat: paginated token cards

--- src/components/common/CardList.vue
+++ src/components/common/CardList.vue
@@ -1,22 +1,63 @@
 <template>
-  <transition-group
-    class="grid grid-flow-cols grid-cols-3 grid-row-2 gap-4"
-    name="scale"
-    tag="div"
-  >
-    <TokenCard
-      @selectToken="SelectToken(token)"
-      @removeToken="removeToken(token)"
-      v-for="(token, idx) in tokens"
-      :key="idx"
-      :token="token"
-      :active="token === selectedToken"
-    />
-  </transition-group>
+  <div class="relative">
+    <transition tag="template" name="scale">
+      <BaseIcon
+        v-if="page > 1"
+        @click="backPage"
+        size="xl"
+        name="chevron-left"
+        class="
+          text-black
+          hover:text-purple-800
+          absolute
+          -left-8
+          top-80
+          cursor-pointer
+        "
+      />
+    </transition>
+    <transition tag="template" name="scale">
+      <span
+        class="text-purple-800 font-medium text-xl absolute -top-4"
+        v-if="tokens.length > 6"
+        >Page: <span class="text-2xl font-bold">{{ page }}</span></span
+      >
+    </transition>
+    <transition-group
+      class="grid grid-flow-cols grid-cols-3 grid-row-2 gap-4"
+      name="scale"
+      tag="div"
+    >
+      <TokenCard
+        @selectToken="SelectToken(token)"
+        @removeToken="removeToken(token)"
+        v-for="(token, idx) in paginatedTokens"
+        :key="idx"
+        :token="token"
+        :active="token === selectedToken"
+      />
+    </transition-group>
+    <transition tag="template" name="scale">
+      <BaseIcon
+        v-if="hasNextPage"
+        @click="nextPage"
+        size="xl"
+        name="chevron-right"
+        class="
+          text-black
+          hover:text-purple-800
+          absolute
+          -right-8
+          bottom-80
+          cursor-pointer
+        "
+      />
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed, watchEffect } from "vue";
 import TokenCard from "@/components/cards/TokenCard.vue";
 export default defineComponent({
   name: "CardList",
@@ -24,7 +65,7 @@ export default defineComponent({
   props: {
     tokens: Array
   },
-  setup(_, { emit }) {
+  setup(props, { emit }) {
     let selectedToken = ref(null);
     function SelectToken(token) {
       selectedToken.value = token;
@@ -35,9 +76,48 @@ export default defineComponent({
       emit("removeToken", token);
     }
 
+    const page = ref(1);
+
+    const startIndex = computed(() => {
+      return (page.value - 1) * 6;
+    });
+    const endIndex = computed(() => {
+      return page.value * 6;
+    });
+
+    const paginatedTokens = computed(() => {
+      //@ts-ignore
+      return props.tokens.slice(startIndex.value, endIndex.value);
+    });
+
+    const hasNextPage = computed(() => {
+      //@ts-ignore
+      return props.tokens.length > endIndex.value;
+    });
+
+    function nextPage() {
+      page.value++;
+    }
+    function backPage() {
+      page.value--;
+    }
+
+    watchEffect(() => {
+      if (paginatedTokens.value.length === 0 && page.value > 1) {
+        page.value -= 1;
+      }
+    });
+
     return {
       selectedToken,
       SelectToken,
+      startIndex,
+      endIndex,
+      page,
+      paginatedTokens,
+      hasNextPage,
+      nextPage,
+      backPage,
       removeToken
     };
   }
